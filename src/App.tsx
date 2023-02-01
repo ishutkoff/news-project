78f1d5b5235e42996731dd93df3820132a281e2c
ishutkoff <ihutkoff@yandex.ru>
2023-02-02 01:35:06 +0300

    Create themes logic. Add theme context & useTheme hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
-import { Suspense } from 'react'
-import { Link } from 'react-router-dom'
-import { Route, Routes } from 'react-router-dom'
-import './index.scss'
-import { AboutPageLazy } from './pages/AboutPage/AboutPageLazy'
-import { MainPageLazy } from './pages/MainPage/MainPageLazy'
-const App = () => {
-	return (
-		<div className='app'>
-			<Link to={'/'}>Main</Link>
-			<Link to={'/about'}>About</Link>
-			<Suspense fallback={<div>Loading...</div>}>
-				<Routes>
-					<Route path={'/about'} element={<AboutPageLazy />} />
-					<Route path={'/'} element={<MainPageLazy />} />
-				</Routes>
-			</Suspense>
-		</div>
-	)
-}
+import { Suspense, useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import "./styles/index.scss";
+import { AboutPageLazy } from "./pages/AboutPage/AboutPageLazy";
+import { MainPageLazy } from "./pages/MainPage/MainPageLazy";
+import { useTheme } from "./theme/useTheme";
 
-export default App
+const App = () => {
+  const { theme, toggleTheme } = useTheme();
+  return (
+    <div className={`app ${theme}`}>
+      <Link to={"/"}>Main</Link>
+      <Link to={"/about"}>About</Link>
+      <button onClick={() => toggleTheme()}>Сменить тему</button>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageLazy />} />
+          <Route path={"/"} element={<MainPageLazy />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/theme/useTheme.ts
@@ -0,0 +1,21 @@
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext";
+import { useContext } from "react";
+
+interface UseThemeResult {
+  toggleTheme: () => void;
+  theme: Theme;
+}
+
+export function useTheme(): UseThemeResult {
+  const { theme, setTheme } = useContext(ThemeContext);
+  const newTheme = theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT;
+
+  const toggleTheme = () => {
+    localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+    setTheme(newTheme);
+  };
+  return {
+    toggleTheme,
+    theme,
+  };
+}
